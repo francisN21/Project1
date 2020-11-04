bc5a8e070687400d9c88a8a6abd98e0caba1170b
Francisco Rones <ininorones@gmail.com>
2020-11-03 20:42:15 -0800

    Update: comments about the objective for the project

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+// ** Multiple arrays from the word api.
+// ** fetch codes from the api
+// ** Event Listeners
+// ** Storage
+
+let noun = [api];
+let verb = [api];
+let subject = [api];
+let adjective = [api];
+let adverb = [api];
+
+let randomChar = [];
+
+let password = [];
+
+
+
+// return value should be something like this
+// password = adverb + verb + randomChar;
+// Gracefullydancing*1#$43!
+// User input can DIY how they want their password
+// example: password = noun + verb + verb + randomChar;
+
+// we need to have a password strength checker that takes the final password value and checks the strength
